08aa58ab322a0cf580eff02e693d87afe8f84e64
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-03-06 17:13:21 -0600

    added button styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
 // global variables
 var savedScores = [];
 var timerVal = 0;
-
-var question = document.querySelector("#question");
+var timeStampEl = document.querySelector("#timer");
+var timerEl = document.querySelector("#startBtn");
+var questionEl = document.querySelector("#question");
 // hides sections
-
 // save scores
 var setSavedScores = function() {
     localStorage.setItem("savedScores", JSON.stringify(savedScores));
 }
+// timer function
+timerEl.addEventListener("click", function() {
+    if (timerVal === 0) {
+       timerVal = setInterval(function () {
+           timeLeft--;
+           timeStampEl.textContent = "Time left: " + timeLeft;
 
+           if (timeLeft <= 0) {
+               clearInterval(timerVal);
+                finishedQuiz();
+                timeStampEl.textContent = "Time's up!";
+           }
+       }, 1000); 
+    }
+})
+// finished the quiz
