b29cffb7d8780880af09f6d7fd5941af3e6ba796
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-03-06 21:38:43 -0600

    added global vars and questions to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,61 @@
 // global variables
+var questionEl = document.querySelector("#question");
+var optionsEl = Array.from(document.querySelector("#answerBtn"));
+var timerEl = document.querySelector("#timer");
+
+let currentQuestion = {};
+let acceptingAnswer = true;
+let timer = 0;
+let availableQuestions = [];
+
+//question array
+let questions = [
+    { //question 2
+        questionEl: 'Which loop allows you to loop through a block of code while a specified condition is true?',
+        choice1:`for`,
+        choice2:`for...in`,
+        choice3:`while`,
+        choice4:`on...while`,
+        answer: 3,
+    },
+    { //question 3
+        questionEl: 'How do you "jump out" a loop of code?',
+        choice1:`break`,
+        choice2:`stop`,
+        choice3:`continue`,
+        choice4:`fullstop`,
+        answer: 1,
+    },
+    { //question 4
+        questionEl: 'Which of the following comparisons operators mean equal of value and type?',
+        choice1:`=`,
+        choice2:`!==`,
+        choice3:`&&`,
+        choice4:`===`,
+        answer: 4,
+    },
+    { //question 5
+        questionEl: 'How do you contain an array?',
+        choice1:`[]`,
+        choice2:`""`,
+        choice3:`{}`,
+        choice4:`()`,
+        answer: 1,
+    },
+    { //question 5
+        questionEl: 'Which HTML element do we use to include external javaScript file?',
+        choice1:`scripting`,
+        choice2:`java`,
+        choice3:`javascript`,
+        choice4:`script`,
+        answer: 4,
+    },
+    { //question 6
+        questionEl: 'Which one of these is the proper way to create a function?',
+        choice1:`function ="var"`,
+        choice2:`function: myFunction()`,
+        choice3:`function = myFunction()`,
+        choice4:`function myFunction()`,
+        answer: 4,
+    },
+]
