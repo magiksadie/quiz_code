f2db7fec4e6a552036a5e91d398431b4c22d1066
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-03-16 12:21:14 -0500

    cleaned code for troubleshooting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
-// global variables
+//global variables
+var startBtnEl = document.querySelector("#startBtn");
 var questionEl = document.querySelector("#question");
 var optionsEl = new Array(document.querySelector("#answerBtn"));
 var timerEl = document.querySelector("#timer");
 
-let currentQuestion = {};
-let acceptingAnswer = true;
-let timer = 0;
-let questionCounter = 0;
-let availableQuestions = [];
+//start quiz
+startBtnEl.addEventListener('click', function() {
+    console.log("Start quiz has been pressed!");
+    getNewQuestion();
+});
 
-//question array
+//select questions
+function getNewQuestion() {
+
+};
+
+console.log('Youve gotten this far!')
+//question and answers array
 let questions = [
     { //question 2
         questionEl: 'Which loop allows you to loop through a block of code while a specified condition is true?',
@@ -66,30 +73,3 @@ let questions = [
         ]
     },
 ]
-var questionAmount = 6;
-//select questions
-startGame = () => {
-    questionCounter = 0;
-    timer = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
-}
-getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > questionAmount) {
-        localStorage.setItem("recentScore", timer);
-        return window.location.assign("/end.html");
-    }
-    questionCounter++;
-
-    var questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionsIndex];
-    questionEl.innerText = currentQuestion.questionEl;
-    
-    optionsEl.forEach(choice => {
-        var number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
-availableQuestions.splice(questionsIndex, 1);
-
-acceptingAnswer = true;
-};
